1a4a183089b8cf981a2016ead11c164dbd822970
肺文 <kevin81319@gmail.com>
2020-01-01 19:12:30 +0800

    db set to postgresql

--- president_vote_taichung/settings.py
+++ president_vote_taichung/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'president_vote_taichung.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'taichung_vote',
+        'USER': 'taichung_vote_admin',
+        'PASSWORD': 'taichungvote2020win',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
